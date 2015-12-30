3e2d28bd2e1c817036e9af6b09fe8d35d5ee7c6e
Thuong Ho <thuong.t.ho@gmail.com>
2015-12-30 00:19:01 -0800

    add passport and created basic user schema

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,19 @@ module.exports = function(config) {
   db.once('open', function callback() {
     console.log('onTrack db opened');
   });
+
+  var userSchema = mongoose.Schema({
+    firstName: String, 
+    lastName: String,
+    username: String
+  });
+  var User = mongoose.model('User', userSchema);
+
+  User.find({}).exec(function(err, collection) {
+    if (collection.length === 0) {
+      User.create({firstName: 'Sam', lastName: 'Ho', username: 'sam'});
+      User.create({firstName: 'Thuong', lastName: 'Ho', username: 'thuong'});
+      User.create({firstName: 'Cong', lastName: 'Trinh', username: 'cong'});
+    }
+  })
 }
