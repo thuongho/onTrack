3b257cfec1f43107350b66251120d5de5cebd45d
Thuong Ho <thuong.t.ho@gmail.com>
2016-01-07 16:45:11 -0800

    refactor user model in mongoose to own User file

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose'),
-    // crypto = require('crypto');
-    encrypt = require('../utilities/encryption')
+    userModel = require('../models/User');
 
 module.exports = function(config) {
   mongoose.connect(config.db);
@@ -10,35 +9,7 @@ module.exports = function(config) {
     console.log('onTrack db opened');
   });
 
-  var userSchema = mongoose.Schema({
-    firstName: String, 
-    lastName: String,
-    username: String,
-    salt: String,
-    hashed_pwd: String,
-    roles: [String]
-  });
-  userSchema.methods = {
-    authenticate: function(passwordToMatch) {
-      return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-    }
-  }
-
-  var User = mongoose.model('User', userSchema);
-
-  User.find({}).exec(function(err, collection) {
-    if (collection.length === 0) {
-      var salt, hash;
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'sam');
-      User.create({firstName: 'Sam', lastName: 'Ho', username: 'sam', salt: salt, hashed_pwd: hash, roles: ['admin']});
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'thuong');
-      User.create({firstName: 'Thuong', lastName: 'Smith', username: 'thuong', salt: salt, hashed_pwd: hash, roles: []});
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'cong');
-      User.create({firstName: 'Cong', lastName: 'Trinh', username: 'cong', salt: salt, hashed_pwd: hash});
-    }
-  });
+  userModel.createDefaultUsers();
+  
 };
 
