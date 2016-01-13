c3a0caf7e83fa24cec4ff30b6e36402833ff1696
Thuong Ho <thuong.t.ho@gmail.com>
2016-01-12 17:17:28 -0800

    use cache to display the goal

--- public/app/track/mvInspirationDetailCtrl.js
+++ public/app/track/mvInspirationDetailCtrl.js
@@ -1,3 +1,10 @@
-angular.module('app').controller('mvInspirationDetailCtrl', ['$scope', 'mvInspiration', '$routeParams', function($scope, mvInspiration, $routeParams) {
-  $scope.inspiration = mvInspiration.get({ _id:$routeParams.id });
+angular.module('app').controller('mvInspirationDetailCtrl', ['$scope', 'mvCachedGoals', '$routeParams', function($scope, mvCachedGoals, $routeParams) {
+  // $scope.inspiration = mvCachedGoals.get({ _id:$routeParams.id });
+  mvCachedGoals.query().$promise.then(function(collection) {
+    collection.forEach(function(inspiration) {
+      if (inspiration._id === $routeParams.id) {
+        $scope.inspiration = inspiration;
+      }
+    })
+  })
 }]);
